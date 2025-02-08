f1de456c4ff5171c593b1edf520affafeeec9ab5
raynabruno <raynabruno@ufl.edu>
2025-02-08 17:02:01 -0500

    lol

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleSidebar() {
     let sidebar = document.getElementById("sidebar");
     if (sidebar.style.display === "none" || sidebar.style.display === "") {
@@ -7,13 +6,3 @@ function toggleSidebar() {
         sidebar.style.display = "none"; // Hide sidebar
     }
 }
-=======
-function toggleSidebar() {
-    let sidebar = document.getElementById("sidebar");
-    if (sidebar.style.display === "none" || sidebar.style.display === "") {
-        sidebar.style.display = "flex"; // Show sidebar
-    } else {
-        sidebar.style.display = "none"; // Hide sidebar
-    }
-}
->>>>>>> d4e5d98ae338e89295640a494ef6c9262cf6b270
